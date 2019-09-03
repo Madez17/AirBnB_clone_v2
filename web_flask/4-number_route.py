0ddb5bd224aafaab7d5fc0656b3c173858e95fbc
Madez <781@holbertonschool.com>
2019-09-03 21:40:11 +0000

    [create]- Template file html and display only when the parameter will be integer

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """script that starts a Flask web application """
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -37,5 +38,12 @@ def python(text):
 def number(n):
     """Func integer """
     return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def page(n):
+    return render_template('5-number.html', number=n)
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
